27fac94e23922e9627efc72d6d0cb652de98203a
Ethan Graham <95376207+etgraham9@users.noreply.github.com>
2025-07-08 23:12:17 -0400

    Laffey Project Prototypes

---
+++ laffey/script.js
@@ -0,0 +1,36 @@
+const map = L.map('map', {
+  center: [20, 0],
+  zoom: 2,
+  zoomControl: false,
+  dragging: false,
+  scrollWheelZoom: false,
+  doubleClickZoom: false,
+  boxZoom: false,
+  keyboard: false,
+  tap: false,
+  touchZoom: false
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+const sections = document.querySelectorAll(".section");
+
+let observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const { lat, lng, zoom } = entry.target.dataset;
+        map.flyTo([lat, lng], zoom, {
+          duration: 2
+        });
+      }
+    });
+  },
+  {
+    threshold: 0.5
+  }
+);
+
+sections.forEach(section => observer.observe(section));
